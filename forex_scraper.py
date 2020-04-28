761380e7057344644bbdcf5a87a83f0e70f64104
Sahilitrix <sahilitrix123@gmail.com>
2020-04-28 19:34:01 -0400

    bug fixes

--- forex_scraper.py
+++ forex_scraper.py
@@ -7,19 +7,6 @@ from arbitrage_algos import ArbitrageAlgorithms
 from visualization import GraphVisualization
 
 class ForexScraper:
-    # Ignore SSL certificate errors
-    #ctx = ssl.create_default_context()
-    #ctx.check_hostname = False
-    #ctx.verify_mode = ssl.CERT_NONE
-
-    # startSymbolsParam = "USD"
-    # convertSymbolsParam = "ETH,DASH,EUR,BNB,BTC,SOL,LINK,XMR,XTZ,XRP,WTC,ENJ,LTC"
-    # apiKeyParam = "6fb8973a560906b569b9cc10a7af360d2a61279a5d692368d61a977d334bf9de"
-    # cryptocompareUrl = 'https://min-api.cryptocompare.com/data/pricemulti?'+'fsyms=' +startSymbolsParam +'&tsyms=' +convertSymbolsParam+'&api_key=' +apiKeyParam
-
-    # USE THIS API INSTEAD
-    # https://exchangeratesapi.io/
-
     # PROGRAM DESIGN
     # CLASS W CONSTRUCTOR AND FUNCS THAT HANDLES RETRIEVING FOREX DATA FROM API AND FORMATTING INTO A 2D MATRIX
     # CLASS W CONSTRUCTOR AND FUNCS THAT TAKES IN MATRIX AND CREATES VISUAL GRAPH
@@ -30,7 +17,7 @@ class ForexScraper:
     #get the initial list of currencies we want to use
     currencyList = []
     url = "http://api.exchangeratesapi.io/2017-07-23?base=USD"
-    connection = urllib.request.urlopen(url)#, context=ctx)
+    connection = urllib.request.urlopen(url)
     jsonObj = json.loads(connection.read())
 
     exchangeDict = jsonObj["rates"]
@@ -47,7 +34,7 @@ class ForexScraper:
     for baseSymbol in currencyList:
         # CREATE A METHOD TO DO API CALLS
         url = "http://api.exchangeratesapi.io/2017-07-23?base=" +baseSymbol
-        connection = urllib.request.urlopen(url)#, context=ctx)
+        connection = urllib.request.urlopen(url)
         jsonObj = json.loads(connection.read())
         exchangeDict = jsonObj["rates"]
         for exchangeSymbol in exchangeDict:
